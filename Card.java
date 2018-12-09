913cf32fa0c787757dc58e5172a9d40f779ccc12
Ramez007 <45740184+Ramez007@users.noreply.github.com>
2018-12-09 21:48:11 +0200

    Create Card.java

---
+++ Card.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pyramid;
+
+import javax.swing.JLabel;
+
+/**
+ *
+ * @author Ramez
+ */
+public class Card 
+{
+    public int Value;
+    public String ImageName;
+    public JLabel Holder;
+}
